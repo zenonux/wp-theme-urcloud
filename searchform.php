1cd0a8da50d2c4ce3360498be405fcc4b2079d13
zenonux@@outlook.com <zenonux@@outlook.com>
2021-10-11 11:18:25 +0800

    搜索增加标签云

--- searchform.php
+++ searchform.php
@@ -5,4 +5,17 @@
         </form>
     </div>
     <a class="sosearch"><i class="fa fa-search"></i></a>
+    <div class="ur-search-tags">
+        <?php wp_tag_cloud(); ?>
+    </div>
 </div>
+
+<script>
+    $('.ur-search-box input').focus(function() {
+        $('.ur-search-tags').fadeIn()
+    }).blur(function() {
+        setTimeout(function() {
+            $('.ur-search-tags').hide()
+        }, 200)
+    })
+</script>
